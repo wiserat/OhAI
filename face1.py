b7220d69c528b4c60b094ecc2956b60ae83129a3
Jonas Tenora <99197211+wiserat@users.noreply.github.com>
2023-04-09 21:15:37 +0200

    Update face1.py

--- face1.py
+++ face1.py
@@ -2,10 +2,20 @@ import cv2
 import numpy as np
 import face_recognition as fr
 from ohbot import *
+import os
 
 
 #start camera capture
 video_capture = cv2.VideoCapture(0)
+
+#load images of known faces
+known_face_names = []
+known_face_encodings = []
+for img in os.listdir("imgs"):
+    image = fr.load_image_file("imgs/" + img)
+    image_face_encoding = fr.face_encodings(image)[0]
+    known_face_encodings.append(image_face_encoding)
+    known_face_names.append(img.split(".")[0])
 
 #just predifining
 bottom_last = 0
@@ -17,9 +27,10 @@ while True:
 
     #get face from frame
     face_locations = fr.face_locations(rgb_frame)
+    face_encodings = fr.face_encodings(rgb_frame, face_locations)
     
     #top, right, bottom, left = face_locations but we are fine with just two values
-    for _, _, bottom, left in face_locations:
+    for (_, _, bottom, left), face_encoding in zip(face_locations, face_encodings):
 
         #ohbot needs values from 0 to 10 so we need to divide these values that are in pixels
         bottom /= 50
@@ -40,8 +51,20 @@ while True:
         else:
             move(HEADNOD, bottom)
             move(HEADTURN, left)
+            
+        #match face with known faces
+        matches = fr.compare_faces(known_face_encodings, face_encoding)
+        fc_distances = fr.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(fc_distances)
+        #if match found
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+            if name == "josh":
+                say("Don't run away Josh, I'm about to suck your toes, yummy")
+            elif name == "jonas":
+                say("Here comes the ladies man")
         
-        #blink so ohbot won't have creepy red eyes
+        #blink so ohbot won't have dryass creepy red eyes
         to_blink += 1
         if to_blink == 100:
             move(LIDBLINK, 1)
